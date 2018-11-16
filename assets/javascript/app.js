1750557a0014dc6d28c53594106972ca793337b7
Fermenting <realferalxanderyell@gmail.com>
2018-11-15 21:22:27 -0800

    question bank of hilarious questions created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,15 +47,16 @@ function countDown() {
 function gameOn() {
 
   //  Start button disappears
-  //  Timer, Questions, & Submit Appear
+  //  divs Timer, Questions, & Submit Appear
   $("#start").css('display', 'none');
   $("#timer").css('display', 'block');
   $("#questions").css('display', 'block');
-  $("submit").css('display', 'block');
+  $("#submit").css('display', 'block');
 
   //  Timer begins counting down
-  var countDown
-
+  countDown();
+ 
+  //questions are populated
 }
 
 //  Game ends when Submit is clicked OR Timer runs out.
@@ -64,23 +65,81 @@ function gameOver() {
 
   //  On game end: 
   //    Questions, Timer, & Submit Disappear
-  $("#timer").css('display', 'none);
-  $("#questions").css('display', ; none');
-  $("submit").css('display', 'none');
+  $("#timer").css('display', 'none');
+  $("#questions").css('display', 'none');
+  $("#submit").css('display', 'none');
 
-  //    Results, Correct, Wrong, & Abstain appear
+  //    Results, Correct, Wrong, Abstain & Again appear
   $("#results").css('display', 'block');
-  $("correct").css('display', 'block');
-  $("wrong").css('display', 'block');
+  $("#correct").css('display', 'block');
+  $("#wrong").css('display', 'block');
+  $("#again").css('display', 'block');
+  
 
   //    Correct, Wrong, & Abstain appear are populated with values from user's choices.
 
-
-
+  $("#results").text('That was a LOT of Questions!');
+  $("#correct").text(numCorrect);
+  $("#wrong").text(numWrong);
 
 }
 
+// function reset(){
+//   location.reload()
+//
+
+//game start and submit buttons
+$("start").on("click", gameOn());
+$("submit").on("click", gameOver());
+
+// upon clicking Again, reset all the things:
+$("again").on("click", location.reload());
+
+//question bank complete with possible answers, & index of correct answer
+
+var jackieTreehorn = {
+ q1={
+  prompt: "What are you doing?",
+ choose: ["Taking a quiz", "Who wants to know?", "Sitting or Standing.", "Chillaxin'."], 
+ correct: 1
+},
+q2={
+prompt: "Where are you doing it?",
+ choose: ["On the computer.", "I'm at home", "I'm at work.", "What if it's none of these?"], 
+ correct: 3
+},
+q3={
+  prompt: "What time is it right now?",
+ choose: ["dot com! #Bojack", "It's x hours & y minutes", "When does the sun set?", "The timer above says 1 minute."], 
+ correct: 2
+},
+q4={
+  prompt: "Why are you here?",
+ choose: ["Here I am!", "Why is anybody anywhere?", "Woah...That's deep.", "I'm thinking about hiring a coder."], 
+ correct: 1
+},
+q5={
+  prompt: "Do you like quizzes?",
+ choose: ["Yes, they give me personality", "No, too many questions.", "I like popcorn, not pop quizzes.", "How can I say this nicely?"], 
+ correct: 3
+},
+q6={
+  prompt: "If you could make a quiz, what would yours be about?",
+ choose: ["Pizza", "Cats", "Gif vs. Jif", "17th Century French Poetry", "Huh?"], 
+ correct: 4
+},
+q7={
+  prompt: "Want an easy one?",
+ choose: ["Yes!", "Do I?", "No!!"], 
+ correct: 1
+},
+q8={
+  prompt: "Have you figured it out?",
+ choose: ["What if I said yes?", "Of course!", "Too Easy.", "Not this time."], 
+ correct: 0
+},
+},
 
 
-$("start").on("click", gameOn())
+
 
