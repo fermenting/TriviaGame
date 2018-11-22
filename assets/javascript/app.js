a060999f1f324ad45181e0b4d412c69e8dd3b39f
Fermenting <realferalxanderyell@gmail.com>
2018-11-22 01:23:25 -0800

    got the labels to display for the radio button, and gave each button an 'i'+ 'j' identifier. Working through code piece by piece to assemble working components.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@
 //    Correct, Wrong, & Abstain appear are populated with values from user's choices.
 
 
-// question bank object
+//question bank complete with possible answers, & index of correct answer
 var jackieTreehorn = {
   questions: [{
     prompt: "What are you doing?",
@@ -58,53 +58,42 @@ var jackieTreehorn = {
 };
 
 // tell the browser to wait for clicks to do stuff:
+$(document).ready(function () {
 
+  //start button starts the game
+  $("#start").on("click", function () {
+    gameOn();
+  });
 
-$("#start").click(function() {
-  gameOn();
-});
-
-$("#submit").click(function() {
-  gameOver();
-});
-
-  // console.log(jackieTreehorn)
-  // console.log(jackieTreehorn.questions)
-
-
-  //game start and submit buttons
-  
+  //   //submit button ends the game
+  //   $("#submit").on("click", function () {
+  //     gameOver();
+  //   });
 
 
   // On click of start button:
   function gameOn() {
 
     //  Start button disappears
-    //  divs Timer, Questions, & Submit Appear
     $("#start").css("display", "none");
+
+    //  divs Timer, Questions, & Submit Appear
     $("#timer").css('display', 'block');
     $("#questions").css('display', 'block');
     $("#submit").css('display', 'block');
 
-    //questions are populated
+    //Questions are populated
     questionMaker();
 
-    //   //  Timer begins counting down
-    //   countDown();
+    //Timer begins counting down
+    // countDown();
 
+    //end of start button
   }
 
-  // //game start and submit buttons
-  // $("start").click(console.log("clack"));
 
-
-
-  $("submit").click(gameOver());
-
-  // // // upon clicking Again, reset all the things:
-  // $("again").on("click", location.reload());
-
-  // //question bank complete with possible answers, & index of correct answer
+  //   // upon clicking Again, reset all the things:
+  //   $("again").on("click", location.reload());
 
 
   function questionMaker() {
@@ -112,46 +101,62 @@ $("#submit").click(function() {
     //loop through the question bank
     for (var i in jackieTreehorn.questions) {
       // console.log(jackieTreehorn.questions[i].prompt)
-      // console.log (jackieTreehorn.questions[i].choose)
-      // console.log (jackieTreehorn.questions[i].correct)
+      console.log(jackieTreehorn.questions[i].choose)
+      console.log(jackieTreehorn.questions[i].correct)
 
       //create a form for each question
-      var newQ = $("<form>");
-      //assign each question the id 'quest'
-      newQ.attr("id", "quest");
+      var questionRadio = $("<form>");
+
+      //assign each question the data-question-number the value of it's index
+      questionRadio.attr("data-question-number", [i]);
+
       //populate the question text
-      newQ.text(jackieTreehorn.questions[i].prompt);
+      questionRadio.text(jackieTreehorn.questions[i].prompt);
+
       //assign each question the correct answer value
-      newQ.attr("data-correct", jackieTreehorn.questions[i].correct);
+      questionRadio.attr("data-correct", jackieTreehorn.questions[i].correct);
+
+      //give questions the ability for unique stylings
+      questionRadio.addClass("question-css")
+
+      questionRadio.append($("<br>"))
 
       //append the question form onto the question div
-      $("#questions").append("<p></p>")
-      $("#questions").append(newQ);
-      $("#questions").append("<p></p>")
+      $("#questions").append(questionRadio);
       // console.log(jackieTreehorn.questions[i].prompt)
       // console.log (jackieTreehorn.questions[i].correct)
+
       //    Questions have radio buttons to select answers
 
       //we need to loop through the answer array
       for (var j in jackieTreehorn.questions[i].choose) {
 
+        //to create a new line for each choice
+        questionRadio.append($("<br>"))
+
+        //assign option text and option-id
+        radLabel = $("<label>")
+        radLabel.addClass("option-css")
+        radLabel.attr("for", [i].toString() + [j].toString());
+        //assign option text
+        radLabel.text(jackieTreehorn.questions[i].choose[j]);
+
+        questionRadio.append(radLabel)
 
         //create each option as an <input>
-        var newA = $("<input>");
+        var radio = $("<input>");
         //assign input to be a radio button
-        newA.attr("type", "radio");
+        radio.attr("type", "radio");
         //assign dem some names
-        newA.attr("name", "name" + i)
+        radio.attr("name", "name" + [i])
         //assign data-choice-number to each according to order it appears
-        newA.attr("value", j);
-        //assign option text
-        newA.attr("id", "id" + i);
-        //assign option text
-        newA.text(jackieTreehorn.questions[i].choose[j]);
+        radio.attr("value", [i].toString() + [j].toString());
+        //give each label unique id
+        radio.attr("id", [i].toString() + [j].toString());
 
 
         //append to question form 
-        newQ.append(newA);
+        questionRadio.append(radio);
         // console.log(jackieTreehorn.questions[i].choose[j])
 
         //end of 'j' loop
@@ -164,118 +169,119 @@ $("#submit").click(function() {
 
 
 
-  //  Timer begins counting down
-  function countDown() {
-    // Set game time to 2 minutes in seconds
-    var gameTime = 120;
+  //   // //  Timer begins counting down
+  //   // function countDown() {
+  //   //   // Set game time to 2 minutes in seconds
+  //   //   var gameTime = 120;
 
-    var tick = setInterval(function () {
+  //   //   var tick = setInterval(function () {
 
-      //decrease gameTime by 1 second until time is out.
-      if (gameTime > 0) {
-        gameTime--;
+  //   //     //decrease gameTime by 1 second until time is out.
+  //   //     if (gameTime > 0) {
+  //   //       gameTime--;
 
-        // convert our seconds to minutes, seconds
-        // Thanks W3schools!
-        var minutes = Math.floor(gameTime % (1000 * 60 * 60) / (1000 * 60));
-        var seconds = Math.floor(gameTime % (1000 * 60) / (1000));
-        $("#timer").innerHTHML = minutes + " minutes : " + seconds + " seconds"
-        console.log(gameTime)
-      }
-      //  Game ends when Submit is clicked OR Timer runs out.
-      else {
-        clearInterval(tick);
-      gameOver();
-      }
-    }, 1000);
-
-
-//keeping score
-
-function checkScore() {
-var correct;
-var wrong;
-var abstain;
+  //   //       // convert our seconds to minutes, seconds
+  //   //       // Thanks W3schools!
+  //   //       var minutes = Math.floor(gameTime % (1000 * 60 * 60) / (1000 * 60));
+  //   //       var seconds = Math.floor(gameTime % (1000 * 60) / (1000));
+  //   //       $("#timer").innerHTHML = minutes + " minutes : " + seconds + " seconds"
+  //   //       console.log(gameTime)
+  //   //     }
+  //   //     //  Game ends when Submit is clicked OR Timer runs out.
+  //   //     else {
+  //   //       clearInterval(tick);
+  //   //       gameOver();
+  //   //     }
+  //   //   }, 1000);
 
 
-if ($("input[name='name0']").val() === jackieTreehorn.questions[0].correct) {
-  correct++;
-}
-if ($("input[name='name0']").val().isNaN() === true ) {
-  abstain++;
-}
-if ($("input[name='name1']").val() === jackieTreehorn.questions[1].correct) {
-  correct++;
-}
-if ($("input[name='name1']").val().isNaN() === true ) {
-  abstain++;
-}
-if ($("input[name='name2']").val() === jackieTreehorn.questions[2].correct) {
-  correct++;
-}
-if ($("input[name='name2']").val().isNaN() === true ) {
-  abstain++;
-}
-if ($("input[name='name3']").val() === jackieTreehorn.questions[3].correct) {
-  correct++;
-}
-if ($("input[name='name3']").val().isNaN() === true ) {
-  abstain++;
-}
-if ($("input[name='name4']").val() === jackieTreehorn.questions[4].correct) {
-  correct++;
-}
-if ($("input[name='name4']").val().isNaN() === true ) {
-  abstain++;
-}
-if ($("input[name='name5']").val() === jackieTreehorn.questions[5].correct) {
-  correct++;
-}
-if ($("input[name='name5']").val().isNaN() === true ) {
-  abstain++;
-}
-if ($("input[name='name6']").val() === jackieTreehorn.questions[6].correct) {
-  correct++;
-}
-if ($("input[name='name6']").val().isNaN() === true ) {
-  abstain++;
-}
-if ($("input[name='name7']").val() === jackieTreehorn.questions[7].correct) {
-  correct++;
-}
-if ($("input[name='name7']").val().isNaN() === true ) {
-  abstain++;
-}
-wrong = 8-correct;
+  //     //keeping score
+
+  //     function checkScore() {
+  //       var correct;
+  //       var wrong;
+  //       var abstain;
 
 
-
-  //  Game ends when Submit is clicked OR Timer runs out.
-  function gameOver() {
-    // clearInterval(tick);
-
-    //  On game end: 
-    //    Questions, Timer, & Submit Disappear
-    $("#timer").css('display', 'none');
-    $("#questions").css('display', 'none');
-    $("#submit").css('display', 'none');
-
-    //    Results, Correct, Wrong, Abstain & Again appear
-    $("#results").css('display', 'block');
-    $("#correct").css('display', 'block');
-    $("#wrong").css('display', 'block');
-    $("#again").css('display', 'block');
-
-    //    Correct, Wrong, & Abstain appear are populated with values from user's choices.
-    $("#results").text('That was a LOT of Questions!');
-    $("#correct").text(correct);
-    $("#wrong").text(wrong);
-    $("#abstain").text(abstain)
-  };
-
-
-  //end of document ready 
+  //       if ($("input[name='name0']").val() === jackieTreehorn.questions[0].correct) {
+  //         correct++;
+  //       }
+  //       if ($("input[name='name0']").val().isNaN() === true) {
+  //         abstain++;
+  //       }
+  //       if ($("input[name='name1']").val() === jackieTreehorn.questions[1].correct) {
+  //         correct++;
+  //       }
+  //       if ($("input[name='name1']").val().isNaN() === true) {
+  //         abstain++;
+  //       }
+  //       if ($("input[name='name2']").val() === jackieTreehorn.questions[2].correct) {
+  //         correct++;
+  //       }
+  //       if ($("input[name='name2']").val().isNaN() === true) {
+  //         abstain++;
+  //       }
+  //       if ($("input[name='name3']").val() === jackieTreehorn.questions[3].correct) {
+  //         correct++;
+  //       }
+  //       if ($("input[name='name3']").val().isNaN() === true) {
+  //         abstain++;
+  //       }
+  //       if ($("input[name='name4']").val() === jackieTreehorn.questions[4].correct) {
+  //         correct++;
+  //       }
+  //       if ($("input[name='name4']").val().isNaN() === true) {
+  //         abstain++;
+  //       }
+  //       if ($("input[name='name5']").val() === jackieTreehorn.questions[5].correct) {
+  //         correct++;
+  //       }
+  //       if ($("input[name='name5']").val().isNaN() === true) {
+  //         abstain++;
+  //       }
+  //       if ($("input[name='name6']").val() === jackieTreehorn.questions[6].correct) {
+  //         correct++;
+  //       }
+  //       if ($("input[name='name6']").val().isNaN() === true) {
+  //         abstain++;
+  //       }
+  //       if ($("input[name='name7']").val() === jackieTreehorn.questions[7].correct) {
+  //         correct++;
+  //       }
+  //       if ($("input[name='name7']").val().isNaN() === true) {
+  //         abstain++;
+  //       }
+  //       wrong = 8 - correct;
 
 
 
-}
+  //       //  Game ends when Submit is clicked OR Timer runs out.
+  //       function gameOver() {
+  //         // clearInterval(tick);
+
+  //         //  On game end: 
+  //         //    Questions, Timer, & Submit Disappear
+  //         $("#timer").css('display', 'none');
+  //         $("#questions").css('display', 'none');
+  //         $("#submit").css('display', 'none');
+
+  //         //    Results, Correct, Wrong, Abstain & Again appear
+  //         $("#results").css('display', 'block');
+  //         $("#correct").css('display', 'block');
+  //         $("#wrong").css('display', 'block');
+  //         $("#again").css('display', 'block');
+
+  //         //    Correct, Wrong, & Abstain appear are populated with values from user's choices.
+  //         $("#results").text('That was a LOT of Questions!');
+  //         $("#correct").text(correct);
+  //         $("#wrong").text(wrong);
+  //         $("#abstain").text(abstain)
+  //       };
+
+
+  //       //end of document ready 
+
+  //     }
+
+  //   // }
+});
